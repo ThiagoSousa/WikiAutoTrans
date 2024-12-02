9d06f650ee62cb039cc3d1b674669ebe23d38fb3
Thiago Silveira <thiago.silveira@giance.ai>
2024-12-02 13:13:00 +0800

    sample user configuration

---
+++ src/config/sample-user-config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+family = 'wikipedia'
+mylang = 'pt'
+usernames['wikipedia']['pt'] = 'Username'
+usernames['wikipedia']['en'] = 'Username'
+usernames['wikidata']['wikidata'] = 'Username'
+
+console_encoding = 'utf-8'
+# use_api_login = True
+password_file = "user-password.py"
